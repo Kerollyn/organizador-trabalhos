12964cb9f07f7745466ac3f4e33a3603996b71a4
thiago <lacerdat25@gmail.com>
2021-04-02 17:44:21 -0300

    Implementação de fluxo de login...

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,38 +1,54 @@
-import React from "react";
-
+import { React, useState } from "react";
 
 import { Container, Aside, BlockInput, Input } from './styles';
+import { useHistory } from "react-router-dom";
+import axios from 'axios';
 
 export default function Login() {
-  
-    function handleSubmit(){
-        
+
+    const [userName, setUser] = useState([]);
+    const [password, setPassword] = useState([]);
+    let history = useHistory();
+
+    function handleSubmit() {
+
+        axios.post('https://heroku-org-trabalhos-api.herokuapp.com/auth/token', {
+            userName,
+            password
+        })
+            .then(res => {
+                console.log(res)
+                console.log(res.data)
+                history.push("/home");
+            })
+            .catch(error => {
+                console.error(error)
+            })
     }
 
     return (
         <Container>
             <Aside>
-                    <form>
-                        <strong>ACESSO AO SISTEMA</strong>
+                <form>
+                    <strong>ACESSO AO SISTEMA</strong>
 
-                        <BlockInput>
-                            <label>E-mail</label>
-                            <Input name='usuario' type='text' />
-                        </BlockInput>
-                        <BlockInput>
-                            <label>Senha</label>
-                            <Input name='senha' type='password' />
-                        </BlockInput>
-                        <br />
+                    <BlockInput>
+                        <label>E-mail</label>
+                        <Input name='user' type='text' value={userName} onChange={e => setUser(e.target.value)} />
+                    </BlockInput>
+                    <BlockInput>
+                        <label>Senha</label>
+                        <Input name='password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
+                    </BlockInput>
+                    <br />
 
-                        <br />
-                                <button type='button'>Entrar</button>
-                                {/* <button type='button'>Cadastrar</button> */}                       
-                      
-                       
-                    </form>
-                </Aside>
+                    <br />
+                    <button type='button' onClick={handleSubmit}>Entrar</button>
+                    {/* <button type='button'>Cadastrar</button> */}
+
+                </form>
+            </Aside>
         </Container>
-        );
-    
+    );
+
 }
