5330ee726b3a01179fd2f5d6ccbe95e20dedebb0
Javier Correa <javier.correa@taglivros.com.br>
2021-04-13 11:26:13 -0300

    Incluir requisição de upload de arquivo.

--- src/pages/Components/Modal/index.js
+++ src/pages/Components/Modal/index.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 import {MdFileUpload } from 'react-icons/md'
 
 import { Container, ModalTeste, ModalConteudo } from './styles'
 
-const Modal = ({ isShowing, hide }) =>
+const fileUpload = ( file ) => {
+    const formData = new FormData()
+    formData.append('file', file, file.name)
+    axios.post(
+        'https://heroku-org-trabalhos-api.herokuapp.com/classworks',
+        formData,
+        {
+            headers:
+                {Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImUzMGI5NjQwLTkyOTItMTFlYi1hZTdjLWVmMjBjMTgxZmIwMSIsInVzZXJOYW1lIjoiamF2aSIsImlhdCI6MTYxNzcxOTIzNH0.sOnNN7Uj2omHqy6eEVJ0IX0_934KNrFp_Izf_hghCxLQqCcfZfnjbCAp1z5SgOjGmA1ARTyMhxFPKqhhy0mKu0_L2cQEv3MDDCczVt7mhPXFDqgV31KaBjb13oh9Y5Ah5glGVe3UADMfz_hB8EBNT7GUeYwiwFPDpHFLOG-qSGHCwRANrgVZngHe_BM6Z67-a8Sx4zfqBulX8fVlHkphhxZrp4v07gPcCFqgOfqGJw0Cvxinv6miKmKGQGV7jPEQf1i9IAb4eU1qWktGy1Vj6KLIdhsOikAlbHJXS-C_cbfLfTjnlwOxrgvopSviomM5NH63XmrdMFSqigCgP-4PCFToxacb4B49wVAvKnai5O6Nj8WIafnENQnVh8ko8ec_ULIgFX9l8PiLkXIDcqHoe0qHhVsNGKrpXC-dY9kviC-L5bF2FWXsFltzUBpOCwyoro-4uUHEXNkCTks_qpNrTMPJ8_7BHQxRmaEjiX-u6z6vYvF5-14vEMDjd0p8a0Nym7_ToFf137b9wHp22NnammTl16LOaG4udnhdb6c7hAsY2Mmnuk97Jowt9nHPeTSRUYd8uovRxCAPZPGW93A46VmDMoHxcKyzHbvvW6vOge1MrX07KoDPhXVV36FLvbcFom_xTmVDthwHQGCP73nc2hjcdrGxUumCkHEuhvRPqGk'}
+        }
+    )
+}
+
+const Modal = ({ isShowing, hide, file, setFile }) =>
     isShowing
         ? ReactDOM.createPortal(
               <Container>
@@ -23,10 +37,10 @@ const Modal = ({ isShowing, hide }) =>
                                         <MdFileUpload size={50}/>
                                   </li>
                                   <li>
-                                        <input type="file"/>
+                                        <input type="file" onChange={e => setFile(e.target.files[0])}/>
                                   </li>
                                   <li>
-                                        <button type="submit">Upload</button>
+                                        <button type="submit" onClick={e => fileUpload(file)}>Upload</button>
                                    </li>
                               </ul>
                           </ModalConteudo>
