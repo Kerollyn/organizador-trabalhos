0d01d32602fbfb4354e4dfd536e2d602bf11db31
kerollyn <https://github.com/kerollyn>
2021-05-05 20:08:10 -0300

    Limpeza do código

--- src/pages/Components/ClassWorkList/styles.js
+++ src/pages/Components/ClassWorkList/styles.js
@@ -1,138 +1,61 @@
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
-// export const Main = styled.div`
-//     display: flex;
-// `
 
-// export const Container = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     width: 100%;
+export const Section = styled.section`
 
-//     header {
-//         height: 30px;
-//         background-color: #fff;
-//         display: flex;
-//         justify-content: space-between;
+    ul {
+        display: flex;
+        flex-direction: column;
 
-//         div.titulo {
-//             display: flex;
-//             align-items: center;
-//             margin-left: 5px;
-//         }
+        li.header-center {
+            display: flex;
+            flex-direction: row;
+            height: 30px;
+            align-items: center;
+            font-size: 12px;
+            font-weight: 700;
+            border: none;
+        }        
+        li {
+            display: flex;
+            flex-direction: row;
+            border: 1px solid #ccc;
+            div {
+                height: 40px;
+                width: 25%;
+                display: flex;
+                align-items: center;
+                padding: 0 10px;
+            }
+            div.buttons{
+                display: flex;
+                justify-content: flex-end;
+                button{
+                    background-color: #9e1a2a;
+                    color: #fff;
+                    border-radius: 4px;
+                    height: 30px;
+                    width: 90px;
+                    margin-left: 10px;
+                }
+            }
+        }        
+    }
+`
 
-//         div.usuario {
-//             display: flex;
-//             align-items: center;
-//             margin-right: 5px;
+export const Button = styled.button`
+    display: flex;
+    align-items: center;
+    border: none;
+    height: 60px;
+    width: 100%;
+    background-color: #fff;
 
-//             label {
-//                 margin-right: 5px;
-//                 font-size: 10px;
-//             }
-//             button{
-//                 border: none;
-//                 background-color: #fff;
-//             }
-//         }
-//     }
-// `
+    li.row-center {
+        cursor: pointer;
+        align-items: center;
+        margin-top: 1px;
+    }
 
-// export const Top = styled.div`
-//     background-color: #9e1a2a;
-//     display: flex;
-//     align-items: center;
-//     justify-content: space-between;
-//     height: 70px;
-
-//     div {
-//         font-family: 'Roboto';
-//         width: 100%;
-//     }
-// `
-
-// export const Section = styled.section`
-//     padding: 20px;
-//     height: 84vh;
-//     background-color: #ededed;
-
-//     form {
-//         height: 60px;
-//         display: flex;
-//         flex-wrap: wrap;
-//         font-size: 12px;
-//         font-weight: 700;
-//         color: #333;
-//         div {
-//             margin-bottom: 20px;
-//             width: 100%;
-
-//             button {
-//                 height: 29px;
-//                 width: 91px;
-//                 cursor: pointer;
-//                 font-size: 11px;
-//                 color: #fff;
-//                 background-color: #9e1a2a;
-//                 background-color: #9e1a2a;
-//                 border-radius: 4px;              
-//             }
-//         }
-//     }
-
-//     ul {
-//         display: flex;
-//         flex-direction: column;
-
-//         li.header-center {
-//             display: flex;
-//             flex-direction: row;
-//             height: 30px;
-//             align-items: center;
-//             font-size: 12px;
-//             font-weight: 700;
-//             border: none;
-//         }        
-//         li {
-//             display: flex;
-//             flex-direction: row;
-//             border: 1px solid #ccc;
-//             div {
-//                 height: 40px;
-//                 width: 25%;
-//                 display: flex;
-//                 align-items: center;
-//                 padding: 0 10px;
-//             }
-//             div.buttons{
-//                 display: flex;
-//                 justify-content: flex-end;
-//                 button{
-//                     background-color: #9e1a2a;
-//                     color: #fff;
-//                     border-radius: 4px;
-//                     height: 30px;
-//                     width: 90px;
-//                     margin-left: 10px;
-//                 }
-//             }
-//         }        
-//     }
-// `
-
-// export const Button = styled.button`
-//     display: flex;
-//     align-items: center;
-//     border: none;
-//     height: 60px;
-//     width: 100%;
-//     background-color: #fff;
-
-//     li.row-center {
-//         cursor: pointer;
-//         align-items: center;
-//         margin-top: 1px;
-//     }
-
-// `
+`
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -119,22 +119,3 @@ export const Section = styled.section`
         }        
     }
 `
-
-// export const Button = styled.button`
-//     display: flex;
-//     align-items: center;
-//     border: none;
-//     height: 60px;
-//     width: 100%;
-//     background-color: #fff;
-
-//     li.row-center {
-//         cursor: pointer;
-//         align-items: center;
-//         margin-top: 1px;
-//         &.selected {
-//             color: #fff;
-//         }
-//     }
-// `
-
