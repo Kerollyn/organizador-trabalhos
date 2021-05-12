9a5e674a72388d465a7993caec1cd27657f3f9a0
kerollyn <https://github.com/kerollyn>
2021-05-11 21:03:04 -0300

    Inserção do modal-detalhes

--- src/pages/Components/ModalDetail/index.js
+++ src/pages/Components/ModalDetail/index.js
@@ -5,61 +5,58 @@ import { Container, ModalTeste, ModalConteudo, BlockInput } from './styles'
 const ModalDetail = ({ isShowing, hide }) =>
     isShowing
         ? ReactDOM.createPortal(
-              <Container>
-                  <div className='modal-fundo' />
-                  <ModalTeste>
-                      <div className='modal'>
-                          <div className='modal-button'>
+            <Container>
+                <div className='modal-fundo' />
+                <ModalTeste>
+                    <div className='modal'>
+                        <div className='modal-button'>
                               <button type='button' onClick={hide}>
                                   <span aria-hidden='true'>&times;</span>
                               </button>
                           </div>
 
-                            <ModalConteudo>
-                                <ul>
-                                    <li>
-                                        <BlockInput>
-                                            <label>Titulo do trabalho</label>
-                                            <label>TESTE</label>
-                                        </BlockInput>
-                                    </li>
-                                    <li>
-                                        <BlockInput>       
-                                            <label>Disciplina</label>
-                                            <label>TESTE</label>
-                                        </BlockInput>
-                                    </li>                                    
-                                    <li>
-                                        <BlockInput>   
-                                            <label>Nome do professor</label>
-                                            <label>TESTE</label>
-                                        </BlockInput>
-                                    </li>
+                        <ModalConteudo>
+                            <ul>
+                                <li>
+                                    <BlockInput>
+                                        <label>Titulo do trabalho</label>
+                                        <label>TESTE</label>
+                                    </BlockInput>
+                                </li>
+                                <li>
+                                    <BlockInput>       
+                                        <label>Disciplina</label>
+                                        <label>TESTE</label>
+                                    </BlockInput>
+                                </li>                                    
+                                <li>
+                                    <BlockInput>   
+                                        <label>Nome do professor</label>
+                                        <label>TESTE</label>
+                                    </BlockInput>
+                                </li>                   
+                                <li>
+                                    <BlockInput>
+                                        <label>Data de entrega</label>
+                                        <label>TESTE</label>
+                                    </BlockInput>
+                                </li>
+                                <li>
+                                    <BlockInput>   
+                                        <label>Status do trabalho</label>
+                                        <label>TESTE</label>
+                                    </BlockInput>
+                                </li>
+                                <li>
+                                    <button>Fechar</button>
+                                </li>                                        
                                     
-                                    <li>
-                                        <BlockInput>
-                                            <label>Data de entrega</label>
-                                            <label>TESTE</label>
-                                        </BlockInput>
-                                    </li>
-
-                                    <li>
-                                        <BlockInput>   
-                                            <label>Status do trabalho</label>
-                                            <label>TESTE</label>
-                                        </BlockInput>
-                                    </li>
-
-                                    <li>
-                                        <button>Fechar</button>
-                                    </li>                                        
-                                    
-                                </ul>
-                            </ModalConteudo>
-                      </div>
-                  </ModalTeste>
-              </Container>,
-              document.body
+                            </ul>
+                        </ModalConteudo>
+                    </div>
+                </ModalTeste>
+            </Container>,
+            document.body
           )
         : null
 
--- src/pages/Components/ModalEdit/index.js
+++ src/pages/Components/ModalEdit/index.js
@@ -5,65 +5,60 @@ import { Container, ModalTeste, ModalConteudo, BlockInput, Input, Select } from 
 const ModalEdit = ({ isShowing, hide }) =>
     isShowing
         ? ReactDOM.createPortal(
-              <Container>
-                  <div className='modal-fundo' />
-                  <ModalTeste>
-                      <div className='modal'>
-                          <div className='modal-button'>
-                              <button type='button' onClick={hide}>
-                                  <span aria-hidden='true'>&times;</span>
-                              </button>
-                          </div>
+            <Container>
+                <div className='modal-fundo' />
+                <ModalTeste>
+                    <div className='modal'>
+                        <div className='modal-button'>
+                            <button type='button' onClick={hide}>
+                                <span aria-hidden='true'>&times;</span>
+                            </button>
+                        </div>
 
-                            <ModalConteudo>
-                                <ul>
-                                    <li>
-                                        <BlockInput>
-                                            <label>Titulo do trabalho</label>
-                                            <Input/>
-                                        </BlockInput>
-                                    </li>
-                                    <li>
-                                        <BlockInput>       
-                                            <label>Disciplina</label>
-                                            <Input/>
-                                        </BlockInput>
-                                    </li>                                    
-                                    <li>
-                                        <BlockInput>   
-                                            <label>Nome do professor</label>
-                                            <Input/>
-                                        </BlockInput>
-                                    </li>
-                                    
-                                    <li>
-                                        <BlockInput>
-                                            <label>Data de entrega</label>
-                                            <Input type="date"/>
-                                        </BlockInput>
-                                    </li>
-
-                                    <li>
-                                        <BlockInput>   
-                                            <label>Status do trabalho</label>
-                                            <Select>
-                                                <option>Em andamento</option>
-                                                <option>Concluido</option>
-                                            </Select>
-                                        </BlockInput>
-                                    </li>
-
-                                    <li>
-                                         <button type="submit" >Salvar</button>
-                                    </li>
-                                        
-                                    
-                                </ul>
-                            </ModalConteudo>
-                      </div>
-                  </ModalTeste>
-              </Container>,
-              document.body
+                        <ModalConteudo>
+                            <ul>
+                                <li>
+                                    <BlockInput>
+                                        <label>Titulo do trabalho</label>
+                                        <Input/>
+                                    </BlockInput>
+                                </li>
+                                <li>
+                                    <BlockInput>       
+                                        <label>Disciplina</label>
+                                        <Input/>
+                                    </BlockInput>
+                                </li>                                    
+                                <li>
+                                    <BlockInput>   
+                                        <label>Nome do professor</label>
+                                        <Input/>
+                                    </BlockInput>
+                                </li>    
+                                <li>
+                                    <BlockInput>
+                                        <label>Data de entrega</label>
+                                        <Input type="date"/>
+                                    </BlockInput>
+                                </li>
+                                <li>
+                                    <BlockInput>   
+                                        <label>Status do trabalho</label>
+                                        <Select>
+                                            <option>Em andamento</option>
+                                            <option>Concluido</option>
+                                        </Select>
+                                    </BlockInput>
+                                </li>
+                                <li>
+                                    <button type="submit" >Salvar</button>
+                                </li>                                  
+                            </ul>
+                        </ModalConteudo>
+                     </div>
+                </ModalTeste>
+            </Container>,
+            document.body
           )
         : null
 
